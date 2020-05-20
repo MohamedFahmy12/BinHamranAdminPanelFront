15133cf621475f01b8e4b7ca54eea48cc8687877
modyf <modyf@DESKTOP-3CROQE8>
2020-05-20 16:38:17 +0200

    ForRCommiting

--- src/app/Component/Reports/branches-trial-balance/branches-trial-balance.component.ts
+++ src/app/Component/Reports/branches-trial-balance/branches-trial-balance.component.ts
@@ -10,7 +10,7 @@ import { DateHelperService } from 'src/app/Helper/date-helper.service';
 import { TranslateService } from '@ngx-translate/core';
 import { NgForm } from '@angular/forms';
 
-declare var Stimulsoft:any;
+declare var Stimulsoft: any;
 @Component({
   selector: 'app-branches-trial-balance',
   templateUrl: './branches-trial-balance.component.html',
@@ -19,10 +19,10 @@ declare var Stimulsoft:any;
 export class BranchesTrialBalanceComponent implements OnInit {
   options: any;
   designer: any;
-  report:any;
+  report: any;
   result: any;
   sDate: any;
-  eDate:any;
+  eDate: any;
   DateHijri: any;
   PortfolioID: number;
   dbIds: string;
@@ -43,139 +43,141 @@ export class BranchesTrialBalanceComponent implements OnInit {
   public iconWaterMarkPort: string = "";
   constructor(private ReportSer: ReportsServiceService, private toastr: ToastrService,
     private router: Router, private datehelp: DateHelperService, private translate: TranslateService) { }
-    ngOnInit() {
-      this.toastr.warning(this.ToastrMsgTranslate("ToastrMsg.Reporttoster"), this.PageName);
-      this.sDate = this.datehelp.GetCurrentDate();
-      this.eDate = this.datehelp.GetCurrentDate();
-      this.BreadCrumTranslate();
-      this.SelectDatabase();
-    }
-    PickCom(event){
-      debugger;
-      this.ComIDS = event[0].COM_BRN_CODE;
-      for(var i = 1; i< event.length ; i++)
-      {
-        var id= event[i].COM_BRN_CODE;
-        this.ComIDS += ','+ id;
-      }
-    }
-    PickAcc(event){
-      debugger;
-      this.AccIDs = event[0].ACC_ID;
-      for(var i = 1; i< event.length ; i++)
-      {
-        var id= event[i].ACC_ID;
-        this.AccIDs += ','+ id;
-      }
-    }
-    pick(event){
-      debugger;
-      this.dbIds = event[0].DatabaseNameId;
-      for(var i = 1; i< event.length ; i++)
-      {
-        debugger;
-        var id= event[i].DatabaseNameId;
-        this.dbIds += ','+ id;
-      }
-      this.SelectBranches();
-      this.SelectAccounts();
-    }
-    BreadCrumTranslate() {
-      debugger;
-      this.translate.get(["ResultOfPortPage.breadcrumb"])
-        .subscribe((translations: string) => {
-          this.PageName = translations["ResultOfPortPage.breadcrumb"];
-        });
-    }
-    // Translate Toastr MSg
-    ToastrMsgTranslate(Msg: string) {
-      this.translate.get([Msg])
-        .subscribe((translations: string) => {
-          this.ToastrMsg = translations[Msg];
-        });
-      return this.ToastrMsg;
-    }
-    Submit(frm: NgForm) {
-      debugger;
-      this.ViewReport();
-    }
-  
-    ViewReport() {
-      debugger;
-      this.sDate = (<HTMLInputElement>document.getElementById("gregDate"))
-        .value ? (<HTMLInputElement>document.getElementById("gregDate")).value : null;
-      this.ReportSer.BranchesTrialBalance(this.sDate,this.ComIDS, this.dbIds).subscribe(
-        (data: Response) => {
-          debugger;
-          this.result = data;
-          this.router.navigate(['/ViewReport', { 'Reportview': this.result }]);
-        },
-        err => {
-          this.toastr.error(this.ToastrMsgTranslate("ToastrMsg.UnExpError"), this.PageName);
-        }
-      );
-    }
-    EditReport() {
-  
-      this.router.navigate(['/editreports', { 'ReportEdit': 'RPTResultOfPortofolioWork.mrt' }]);
-      debugger;
-    }
-  
-    onSelectPortfolio(selectedItem: any, modalId: any) {
-      debugger;
-      this.PortfolioID = selectedItem.PortfolioID ? selectedItem.PortfolioID : 0;
-      this.PortfolioNameARtxt = selectedItem.NameAR ? selectedItem.NameAR : '';
-    }
-  
-    SelectDatabase() {
-      debugger;
-      this.ReportSer.GetDbNames().subscribe(
-        res => {
-          // this.portfolios=res as Portfolio[] ;
-          debugger;
-          this.DbNames = res as DatabaseModule[];
-        },
-        err => {
-          this.toastr.error(this.ToastrMsgTranslate("ToastrMsg.UnExpError"), this.PageName);
-  
-        }
-      );
-    }
-    SelectAccounts() {
-      debugger;
-  
-      this.ReportSer.GetAccounts(this.dbIds).subscribe(
-        res => {
-          // this.portfolios=res as Portfolio[] ;+
-          this.Accounts = res as AccountModule[];
-        },
-        err => {
-          this.toastr.error(this.ToastrMsgTranslate("ToastrMsg.UnExpError"), this.PageName);
-        }
-      );
-    }
-    SelectBranches() {
-      debugger;
-      this.ReportSer.GetCompanyBranches(this.dbIds).subscribe(
-        res => {
-          // this.portfolios=res as Portfolio[] ;
-          this.Branches = res as BranchModule[];
-        },
-        err => {
-          this.toastr.error(this.ToastrMsgTranslate("ToastrMsg.UnExpError"), this.PageName);
-  
-        }
-      );
-    }
-    ViewReportDesign(){
-      this.options = new Stimulsoft.Designer.StiDesignerOptions();
-      this.options.appearance.fullScreenMode = false;
-      this.designer = new Stimulsoft.Designer.StiDesigner(this.options,"StiDesigner",false);
-      this.report = new Stimulsoft.Report.StiReport();
-      this.report.loadFile('/reports/BranchesTrialBalance.mrt');
-      this.designer.report = this.report;
-      this.designer.renderHtml("designer");
+  ngOnInit() {
+    this.toastr.warning(this.ToastrMsgTranslate("ToastrMsg.Reporttoster"), this.PageName);
+    this.sDate = this.datehelp.GetCurrentDate();
+    this.eDate = this.datehelp.GetCurrentDate();
+    this.BreadCrumTranslate();
+    this.SelectDatabase();
   }
-  
+  PickCom(event) {
+    debugger;
+    this.ComIDS = event[0].COM_BRN_CODE;
+    for (var i = 1; i < event.length; i++) {
+      var id = event[i].COM_BRN_CODE;
+      this.ComIDS += ',' + id;
+    }
+  }
+  PickAcc(event) {
+    debugger;
+    this.AccIDs = event[0].ACC_ID;
+    for (var i = 1; i < event.length; i++) {
+      var id = event[i].ACC_ID;
+      this.AccIDs += ',' + id;
+    }
+  }
+  pick(event) {
+    debugger;
+    this.dbIds = event[0].DatabaseNameId;
+    for (var i = 1; i < event.length; i++) {
+      debugger;
+      var id = event[i].DatabaseNameId;
+      this.dbIds += ',' + id;
+    }
+    this.SelectBranches();
+    this.SelectAccounts();
+  }
+  BreadCrumTranslate() {
+    debugger;
+    this.translate.get(["ResultOfPortPage.breadcrumb"])
+      .subscribe((translations: string) => {
+        this.PageName = translations["ResultOfPortPage.breadcrumb"];
+      });
+  }
+  // Translate Toastr MSg
+  ToastrMsgTranslate(Msg: string) {
+    this.translate.get([Msg])
+      .subscribe((translations: string) => {
+        this.ToastrMsg = translations[Msg];
+      });
+    return this.ToastrMsg;
+  }
+  Submit(frm: NgForm) {
+    debugger;
+    this.ViewReport();
+  }
+
+  ViewReport() {
+    debugger;
+    this.sDate = (<HTMLInputElement>document.getElementById("gregDate"))
+      .value ? (<HTMLInputElement>document.getElementById("gregDate")).value : null;
+    this.ReportSer.BranchesTrialBalance(this.sDate, this.ComIDS, this.dbIds).subscribe(
+      (data: Response) => {
+        debugger;
+        this.result = data;
+        this.router.navigate(['/ViewReport', { 'Reportview': this.result }]);
+      },
+      err => {
+        this.toastr.error(this.ToastrMsgTranslate("ToastrMsg.UnExpError"), this.PageName);
+      }
+    );
+  }
+  EditReport() {
+
+    this.router.navigate(['/editreports', { 'ReportEdit': 'RPTResultOfPortofolioWork.mrt' }]);
+    debugger;
+  }
+
+  onSelectPortfolio(selectedItem: any, modalId: any) {
+    debugger;
+    this.PortfolioID = selectedItem.PortfolioID ? selectedItem.PortfolioID : 0;
+    this.PortfolioNameARtxt = selectedItem.NameAR ? selectedItem.NameAR : '';
+  }
+
+  SelectDatabase() {
+    debugger;
+    this.ReportSer.GetDbNames().subscribe(
+      res => {
+        // this.portfolios=res as Portfolio[] ;
+        debugger;
+        this.DbNames = res as DatabaseModule[];
+      },
+      err => {
+        this.toastr.error(this.ToastrMsgTranslate("ToastrMsg.UnExpError"), this.PageName);
+
+      }
+    );
+  }
+  SelectAccounts() {
+    debugger;
+
+    this.ReportSer.GetAccounts(this.dbIds).subscribe(
+      res => {
+        // this.portfolios=res as Portfolio[] ;+
+        this.Accounts = res as AccountModule[];
+      },
+      err => {
+        this.toastr.error(this.ToastrMsgTranslate("ToastrMsg.UnExpError"), this.PageName);
+      }
+    );
+  }
+  SelectBranches() {
+    debugger;
+    this.ReportSer.GetCompanyBranches(this.dbIds).subscribe(
+      res => {
+        // this.portfolios=res as Portfolio[] ;
+        this.Branches = res as BranchModule[];
+      },
+      err => {
+        this.toastr.error(this.ToastrMsgTranslate("ToastrMsg.UnExpError"), this.PageName);
+
+      }
+    );
+  }
+  ViewReportDesign() {
+    this.options = new Stimulsoft.Designer.StiDesignerOptions();
+    this.options.appearance.fullScreenMode = false;
+    this.designer = new Stimulsoft.Designer.StiDesigner(this.options, "StiDesigner", false);
+    this.report = new Stimulsoft.Report.StiReport();
+    this.report.loadFile('/reports/BranchesTrialBalance.mrt');
+    this.designer.report = this.report;
+    this.designer.renderHtml("designer");
+    this.designer.onSaveReport = function (args) {
+      var jsonReport = args.report.saveToJsonString();
+      
+    }
+  }
+
+
 
 }
